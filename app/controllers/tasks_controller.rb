161d2ad9808056542278d729de687be4773445d2
Adil Ismail <adilismail62@gmail.com>
2022-10-23 14:04:40 +0530

    Setup rubocop linter

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,15 @@
-class TasksController < ApplicationController
-    # respond_to :html,:xml,:json
+# frozen_string_literal: true
 
-    def index
-        @tasks = Task.all
-        # respond_with(@tasks)
-        respond_to do |format|
-            format.html
-            format.json { render json: @tasks }
-            format.xml { render xml: @tasks }
-          end
-    end
+class TasksController < ApplicationController
+  # respond_to :html,:xml,:json
+
+  def index
+    @tasks = Task.all
+    # respond_with(@tasks)
+    respond_to do |format|
+        format.html
+        format.json { render json: @tasks }
+        format.xml { render xml: @tasks }
+      end
+  end
 end
